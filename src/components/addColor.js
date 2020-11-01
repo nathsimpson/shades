200eaf611b897a6f8585af2213b14a841ff79d99
Nathan Simpson <hello@nathansimpson.design>
2020-11-01 14:35:43 +1100

    Add text input to addColor component

--- src/components/addColor.js
+++ src/components/addColor.js
@@ -1,46 +1,65 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import { Button } from './button';
+import { colors as themeColors } from '../theme';
 
 export const AddColor = ({ setColors, colors, newColor, setNewColor }) => (
   <div css={{ display: 'flex', alignItems: 'center' }}>
-    <label>Choose color:</label>
+    <label css={{ color: themeColors[600], marginRight: 6 }}>Add color:</label>
+
+    <input
+      value={newColor}
+      onChange={({ target: { value: newColor } }) => setNewColor(newColor)}
+      placeholder="Enter a color"
+      css={{
+        marginRight: 6,
+        paddingLeft: 12,
+        background: 'none',
+        outline: 'none',
+        border: `2px solid ${themeColors[600]}`,
+        height: 24,
+        color: themeColors[900],
+        borderRadius: 24,
+        letterSpacing: 1
+      }}
+    />
+
     <input
       type="color"
-      placeholder="Add Color"
       value={newColor}
       onChange={({ target: { value: newColor } }) => setNewColor(newColor)}
       css={{
-        margin: '0 6px',
-        padding: 0,
+        marginRight: 6,
+        padding: 3,
         background: 'none',
         outline: 'none',
-        border: '2px solid white',
-        height: 30,
-        width: 30,
-        borderRadius: 30,
+        border: `2px solid ${themeColors[600]}`,
+        height: 24,
+        width: 24,
+        borderRadius: 24,
+
+        '&:hover': {
+          border: `2px solid ${themeColors[900]}`
+        },
 
         '::-webkit-color-swatch-wrapper': {
           background: 'none',
           padding: 0,
-          height: 30,
-          width: 30
-        },
-
-        '::-webkit-color-swatch:hover': {
-          opacity: 0.5
+          height: 24,
+          width: 24
         },
 
         '::-webkit-color-swatch': {
           border: 'none',
           background: 'none',
           padding: 0,
-          height: 30,
-          width: 30,
+          height: 24,
+          width: 24,
           borderRadius: 15
         }
       }}
     />
+
     <Button onClick={() => setColors(colors.concat([newColor]))} label="Add" />
   </div>
 );
