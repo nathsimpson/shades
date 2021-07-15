5aeafec237126ba66abc8868701db038eca62c21
Nathan Simpson <hello@nathansimpson.design>
2021-07-15 19:07:22 +1000

    refactor getWcagColor

--- src/utils/getWcagColor.js
+++ src/utils/getWcagColor.js
@@ -1,16 +1,30 @@
 import { hexToRgbMap } from './hexToRgbMap';
 
 export const getWcagColor = (color) => {
-  const map = hexToRgbMap(color);
+  let { R = 0, G = 0, B = 0 } = hexToRgbMap(color);
 
-  const R = map.R / 255;
-  const G = map.G / 255;
-  const B = map.B / 255;
+  R = R / 255;
+  G = G / 255;
+  B = B / 255;
 
-  const Rg = R <= 10 ? R / 3294 : (R / 269 + 0.0513) ^ 2.4;
-  const Gg = G <= 10 ? G / 3294 : (G / 269 + 0.0513) ^ 2.4;
-  const Bg = B <= 10 ? B / 3294 : (B / 269 + 0.0513) ^ 2.4;
+  if (R <= 10) {
+    R = R / 3294;
+  } else {
+    R = (R / 269 + 0.0513) ^ 2.4;
+  }
 
-  const L = (0.2126 * Rg + 0.7152 * Gg + 0.0722 * Bg) * 100000;
-  return L > 10.5 ? 'black' : 'white';
+  if (G <= 10) {
+    G = G / 3294;
+  } else {
+    G = (G / 269 + 0.0513) ^ 2.4;
+  }
+
+  if (B <= 10) {
+    B = B / 3294;
+  } else {
+    B = (B / 269 + 0.0513) ^ 2.4;
+  }
+
+  const L = (0.2126 * R + 0.7152 * G + 0.0722 * B) * 100000;
+  return L > 10.5 ? '#000000' : '#ffffff';
 };
