571adda4f59f1baf86ec80cf0093b0c3cdb2dcd9
Nathan Simpson <hello@nathansimpson.design>
2021-11-04 08:52:22 +1100

    Update tests

--- src/utils/arrayToPalette/index.test.js
+++ src/utils/arrayToPalette/index.test.js
@@ -3,29 +3,27 @@ const { arrayToPalette } = require('./index');
 test(`expects pallete JSON to be generated for 11 colors`, () => {
   expect(
     arrayToPalette([
-      '#040703',
-      '#182c11',
-      '#2a4e1d',
-      '#3d732b',
-      '#4f9438',
-      '#63b946',
-      '#82c76b',
-      '#9ed48c',
-      '#bde2b1',
-      '#daeed3',
-      '#f9fcf8'
+      { lightness: 50, value: '#040703' },
+      { lightness: 100, value: '#182c11' },
+      { lightness: 200, value: '#2a4e1d' },
+      { lightness: 300, value: '#3d732b' },
+      { lightness: 400, value: '#4f9438' },
+      { lightness: 500, value: '#63b946' },
+      { lightness: 600, value: '#82c76b' },
+      { lightness: 700, value: '#9ed48c' },
+      { lightness: 800, value: '#bde2b1' },
+      { lightness: 900, value: '#daeed3' }
     ])
   ).toStrictEqual({
-    100: '#040703',
-    1000: '#daeed3',
-    1100: '#f9fcf8',
-    200: '#182c11',
-    300: '#2a4e1d',
-    400: '#3d732b',
-    500: '#4f9438',
-    600: '#63b946',
-    700: '#82c76b',
-    800: '#9ed48c',
-    900: '#bde2b1'
+    50: '#040703',
+    100: '#182c11',
+    200: '#2a4e1d',
+    300: '#3d732b',
+    400: '#4f9438',
+    500: '#63b946',
+    600: '#82c76b',
+    700: '#9ed48c',
+    800: '#bde2b1',
+    900: '#daeed3'
   });
 });
