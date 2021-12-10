620adc79a6632904ef4224017647266a057dc1c1
Nathan Simpson <hello@nathansimpson.design>
2021-12-10 11:32:34 +1100

    lowercase h,s,l references

--- src/utils/rgbMapToHslMap/index.test.js
+++ src/utils/rgbMapToHslMap/index.test.js
@@ -3,7 +3,7 @@ const { rgbMapToHslMap } = require('./index');
 const { hexToRgbMap } = require('../hexToRgbMap');
 const { exampleColors } = require('../testHelpers');
 
-export const formatHSL = ({ H, S, L }) => `hsl(${H}, ${S}%, ${L}%)`;
+export const formatHSL = ({ h, s, l }) => `hsl(${h}, ${s}%, ${l}%)`;
 
 export const hexToHSL = (input) => {
   const rgbMap = hexToRgbMap(input);
@@ -15,21 +15,21 @@ export const hexToHSL = (input) => {
 // expects correct Hue
 exampleColors.forEach(({ name, rgbObj, hslObj }) => {
   test(`expects correct Hue for ${name}`, () => {
-    expect(rgbMapToHslMap(rgbObj).H).toBe(hslObj.H);
+    expect(rgbMapToHslMap(rgbObj).h).toBe(hslObj.h);
   });
 });
 
 // expects correct Saturation
 exampleColors.forEach(({ name, rgbObj, hslObj }) => {
   test(`expects correct Saturation for ${name}`, () => {
-    expect(rgbMapToHslMap(rgbObj).S).toBe(hslObj.S);
+    expect(rgbMapToHslMap(rgbObj).s).toBe(hslObj.s);
   });
 });
 
 // expects correct Lightness
 exampleColors.forEach(({ name, rgbObj, hslObj }) => {
   test(`expects correct Lightness for ${name}`, () => {
-    expect(rgbMapToHslMap(rgbObj).L).toBe(hslObj.L);
+    expect(rgbMapToHslMap(rgbObj).l).toBe(hslObj.l);
   });
 });
 
--- src/utils/testHelpers.js
+++ src/utils/testHelpers.js
@@ -1,70 +1,70 @@
 export const exampleColors = [
   {
     name: 'Orange',
-    hslObj: { H: 26, S: 99, L: 49 },
+    hslObj: { h: 26, s: 99, l: 49 },
     hex: '#fa6d01',
     hsl: 'hsl(26, 99%, 49%)',
     rgbObj: { R: 250, G: 109, B: 1 }
   },
   {
     name: 'Dark purple',
-    hslObj: { H: 331, S: 24, L: 22 },
+    hslObj: { h: 331, s: 24, l: 22 },
     hex: '#462b38',
     hsl: 'hsl(331, 24%, 22%)',
     rgbObj: { R: 70, G: 43, B: 56 }
   },
   {
     name: 'Brown',
-    hslObj: { H: 28, S: 63, L: 34 },
+    hslObj: { h: 28, s: 63, l: 34 },
     hex: '#8e5420',
     hsl: 'hsl(28, 63%, 34%)',
     rgbObj: { R: 142, G: 84, B: 32 }
   },
   {
     name: 'Burgundy',
-    hslObj: { H: 345, S: 89, L: 26 },
+    hslObj: { h: 345, s: 89, l: 26 },
     hex: '#7e0724',
     hsl: 'hsl(345, 89%, 26%)',
     rgbObj: { R: 126, G: 7, B: 36 }
   },
   {
     name: 'Green',
-    hslObj: { H: 127, S: 99, L: 32 },
+    hslObj: { h: 127, s: 99, l: 32 },
     hex: '#01a313',
     hsl: 'hsl(127, 99%, 32%)',
     rgbObj: { R: 1, G: 163, B: 19 }
   },
   {
     name: 'Light blue',
-    hslObj: { H: 191, S: 75, L: 66 },
+    hslObj: { h: 191, s: 75, l: 66 },
     hex: '#67d1e9',
     hsl: 'hsl(191, 75%, 66%)',
     rgbObj: { R: 103, G: 209, B: 233 }
   },
   {
     name: 'Pink',
-    hslObj: { H: 333, S: 96, L: 56 },
+    hslObj: { h: 333, s: 96, l: 56 },
     hex: '#fb2284',
     hsl: 'hsl(333, 96%, 56%)',
     rgbObj: { R: 251, G: 34, B: 132 }
   },
   {
     name: 'Turquoise',
-    hslObj: { H: 148, S: 100, L: 54 },
+    hslObj: { h: 148, s: 100, l: 54 },
     hex: '#14ff82',
     hsl: 'hsl(148, 100%, 54%)',
     rgbObj: { R: 20, G: 255, B: 130 }
   },
   {
     name: 'Slate',
-    hslObj: { H: 217, S: 6, L: 25 },
+    hslObj: { h: 217, s: 6, l: 25 },
     hex: '#3c3f44',
     hsl: 'hsl(217, 6%, 25%)',
     rgbObj: { R: 60, G: 63, B: 68 }
   },
   {
     name: 'Black',
-    hslObj: { H: 0, S: 0, L: 0 },
+    hslObj: { h: 0, s: 0, l: 0 },
     hex: '#000000',
     hsl: 'hsl(0, 0%, 0%)',
     rgbObj: { R: 0, G: 0, B: 0 }
